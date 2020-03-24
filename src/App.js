8384e13a443d4d49b24e9bb931ea10c639404406
vasim <jamadarvasim@outlook.com>
2020-03-23 21:10:15 -0700

    First Commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.js';
+import '../node_modules/font-awesome/css/font-awesome.css';
+import ban11 from './ban11.jpg'
+
+
+function App() {
+  return(
+     <div id="navbar">
+       
+       <span id="main1">Back </span>
+       <i class="fa fa-download" aria-hidden="true" id="download">Download</i>
+     </div>
+     
+     
+   
+  );
+ 
+}
+
+
+export default App;
+
